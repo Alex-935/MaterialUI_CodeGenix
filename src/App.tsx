1b0f625d4e2c968dca3ed9eb52cc4e90e43c525b
Alex-935 <alex.potter3@hotmail.co.uk>
2026-05-14 07:13:49 +0100

    Merge COnflict Resolved

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@
 import React, {useState} from 'react';
 import {Button, CssBaseline, TextField, Slider, Stack, 
   Container} from "@mui/material";
+import AutoAwesomeRoundedIcon from '@mui/icons-material/AutoAwesomeRounded';
 
 function App() {
 
@@ -9,10 +10,11 @@ function App() {
 
   // MUI Spacing is 8px by default
   return ( 
-    <Container maxWidth="xs">
-      <Stack gap={2}>
+    <>
+      <Container maxWidth="xs">{/*Centers everything horizontally*/}
+        <Stack gap={2}>{/* stack stacks things vertically, gap is in increments of 8px*/}
         <CssBaseline />
-        <Button variant="contained">Hello World</Button>
+          <Button startIcon={<AutoAwesomeRoundedIcon />} variant="contained">Hello World</Button>
         <TextField value={value} onChange={(e) => {
           setValue(e.target.value);
           }} 
@@ -20,7 +22,26 @@ function App() {
         />
         <Slider />
       </Stack>
+        <AutoAwesomeRoundedIcon fontSize="small" color="error"/>
+      </Container>
+      
+      <CssBaseline />
+      <Container sx={{height:'100vh', display:'flex',justifyContent:'center',alignItems:'center'}}
+      maxWidth="xs">
+        <Stack gap={2}>
+          <Slider sx={{width:100,color:'success.main', ":hover": {backgroundColor: "yellow"},
+            /* When selecting a class, use an & which is equivalent to the root of the component*/
+            "& .MuiSlider-thumb": {backgroundColor:'red', ":hover" :{backgroundColor: 'blue'}},
+            /* there are certain properties that the browser doesnt recognise such as disabled,
+               error, focused, etc, so Mui-disabled has to be used instead 
+            there is no space between &.Mui-disables because it sits higher on the hierarchy
+            than MuiSlider-thumb */
+            "&.Mui-disabled": { "& .MuiSlider-thumb": {backgroundColor: "green"}},
+            "&.Mui-disabled .MuiSlider-rail": {backgroundColor: "error.main"},
+          }}/>
+        </Stack>
     </Container>
+    </>
   );
 }
 
